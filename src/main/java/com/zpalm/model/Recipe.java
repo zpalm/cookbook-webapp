90fac497139fa2407ad97c3ee522be0413997bb0
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-12-05 17:33:28 +0100

    Improved Model class, added lombok

--- src/main/java/com/zpalm/model/Recipe.java
+++ src/main/java/com/zpalm/model/Recipe.java
@@ -1,34 +1,15 @@
 package com.zpalm.model;
 
 import java.util.List;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class Recipe {
 
     private final Long id;
     private final String name;
     private final List<String> ingredients;
     private final List<String> entry;
-
-    public Recipe(Long id, String name, List<String> ingredients, List<String> entry) {
-        this.id = id;
-        this.name = name;
-        this.ingredients = ingredients;
-        this.entry = entry;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public List<String> getIngredients() {
-        return ingredients;
-    }
-
-    public List<String> getEntry() {
-        return entry;
-    }
 }
