780e6ad0dbd0cc1bf0f61b36beac0e9518dd0066
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2020-03-11 17:38:06 +0100

    Implementing a frontend way to add new recipes

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -26,178 +26,215 @@ class App extends React.Component {
     }
 
     addRecipe(recipe) {
-            let update = this.props.update;
-            axios.post('/recipes', {
+        axios.post('/recipes', {
             name: recipe.name,
-            steps: recipe.steps})
-                .then(res=>{
-                    client({method: 'GET', path: '/invoices'}).done(response => {
-                        this.props.update(response.entity);
-                    });
-                    $.notify("Recipe added", "success");
-                    //window.location = "/retrieve" //This line of code will redirect you once the submission is succeed
-                })
+            steps: recipe.steps,
+            ingredients: recipe.ingredients})
+        .then(res=>{
+            client({method: 'GET', path: '/recipes'}).done(response => {
+                this.updateRecipes(response.entity);
+            });
+             $.notify("Recipe added", "success");
+        })
     }
 
 	render() {
-    		return (
-    		<div class="container-fluid">
-    		    <div class="row">
-    		        <AddRecipe addRecipe={this.addRecipe} update={this.updateRecipes}/>
-    		    </div>
-    		    <div class="row">
-    		        <RecipeList recipes={this.state.recipes} update={this.updateRecipes}/>
-    		    </div>
-    		</div>
-    		)
-    	}
+	    return (
+	        <div class="container-fluid">
+	            <div class="row">
+	                <AddRecipe addRecipe={this.addRecipe} update={this.updateRecipes}/>
+	            </div>
+	            <div class="row">
+	                <RecipeList recipes={this.state.recipes} update={this.updateRecipes}/>
+	            </div>
+	        </div>
+	    )
+	}
 }
 
 class AddRecipe extends React.Component{
+
     constructor(props){
         super(props);
         this.state = {
             id: '',
             name: '',
-            ingredients: '',
+            ingredientType: '',
+            unit: '',
+            quantity: '',
+            ingredients: [{ingredientType: '',  unit: '', quantity: ''}],
             step: '',
             steps: [{step: ''}],
         };
     }
 
     componentDidMount(){
-           this.textarea.focus();
-           autosize(this.textarea);
-        }
+        this.textarea.focus();
+        autosize(this.textarea);
+        $('#addRecipeModal').on('hidden.bs.modal', function () {
+            $(this).find("input,textarea,select").val([]).end();
+        });
+    }
 
     handleSubmit(event) {
         event.preventDefault();
         var recipe = {
-          id: this.state.id,
-          name: this.state.name,
-          ingredients: this.state.ingredients,
-          steps: this.state.steps
+            id: this.state.id,
+            name: this.state.name,
+            ingredients: this.state.ingredients,
+            steps: this.state.steps
         };
         this.props.addRecipe(recipe);
     }
 
-    handleChange(event) {
-        this.setState({[event.target.name]: event.target.value});
-    }
-
-    /*handleStepChange(event) {
+    handleClose(event) {
+        event.preventDefault();
         this.setState({
-        step: event.target.value
+            name: '',
+            ingredientType: '',
+            unit: '',
+            quantity: '',
+            ingredients: [{ingredientType: '',  unit: '', quantity: ''}],
+            step: '',
+            steps: [{step: ''}],
         });
-    }*/
-   handleStepChange(event, i) {
-        /*const newSteps = this.state.steps.map((item, idx) =>
-            {
-           const newData = idx === i ? event.target.value : {};
-
-           return {
-           ...item,
-           ...newData};
-        });
-        this.setState({newSteps});*/
-        const newSteps = this.state.steps.map((recipeStep, idx) => {
-              if (i !== idx) return recipeStep;
-              return { ...recipeStep, step: event.target.value };
-            });
-
-            this.setState({ steps: newSteps });
     }
 
-    /*handleAddStep(event) {
-        this.setState({steps: this.state.steps.concat([{step: ""}])
+    handleNameChange(event) {
+        const newName = event.target.value;
+        this.setState({name: newName});
+    }
+
+    handleStepChange(event, i) {
+        const newSteps = this.state.steps.map((recipeStep, idx) => {
+            if (i !== idx) return recipeStep;
+            return { ...recipeStep, step: event.target.value };
         });
-    }*/
+        this.setState({ steps: newSteps });
+    }
+
+    handleIngredientChange(event, i) {
+        const newSteps = this.state.ingredients.map((recipeStep, idx) => {
+            if (i !== idx) return recipeStep;
+            return { ...recipeStep, [event.target.name]: event.target.value };
+        });
+        this.setState({ ingredients: newSteps });
+    }
 
     handleAddStep() {
-            /*this.setState(state => {
-                const steps = state.steps.concat({step: value});
-                return {
-                    steps,
-                    step: '',
-                };
-            });*/
-            this.setState({
-                  steps: this.state.steps.concat([{ step: "" }])
-                });
-            /*const newSteps = this.state.steps.concat([{step: this.state.step}]);
-            this.setState({steps: newSteps});*/
-            //this.setState({steps: this.state.steps.concat([{step: this.state.step}])})
-        }
+        this.setState({
+            steps: this.state.steps.concat([{ step: "" }])
+        });
+    }
 
-        handleRemoveStep(i) {
+    handleAddIngredient() {
+        this.setState({
+            ingredients: this.state.ingredients.concat([{ingredientType: '',  unit: '', quantity: ''}])
+        });
+    }
+
+    handleRemoveStep(i) {
         this.setState({
               steps: this.state.steps.filter((step, idx) => i !== idx)
-            });
-        }
+        });
+    }
+
+    handleRemoveIngredient(i) {
+        this.setState({
+            ingredients: this.state.ingredients.filter((step, idx) => i !== idx)
+        });
+    }
 
     render() {
-    const style = {
-                    maxHeight:'75px',
-                    minHeight:'38px',
-                      resize:'none',
-                      padding:'9px',
-                      boxSizing:'border-box',
-                      fontSize:'15px'};
-            return (
-                <React.Fragment>
-                    <div class="modal fade" id={"addRecipeModal"} tabindex="-1" role="dialog">
-                      <div class="modal-dialog modal-xl" role="document">
+        return (
+            <React.Fragment>
+                <div class="modal fade" id="addRecipeModal" tabindex="-1" role="dialog">
+                    <div class="modal-dialog modal-xl" role="document">
                         <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title">Hello</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                          </div>
-                          <div class="modal-body">
-                          <div class="container-fluid">
-                            <form>
-                            <div class="row">
-                            <div class="col-md-4">
-                                <div class="md-form">
-                                  <input type="text" id="form1" placeholder="Name" class="form-control" name="name" onChange={event => this.handleChange(event)}/>
-                                </div>
-                                </div>
-                                </div>
-                                {this.state.steps.map((recipeStep, i) => (
-                                <div class="row">
-                                <div class="col-md-6">
-                                <div class="input-group mb-3">
-                                    <textarea key={i} type="text" placeholder={`Step #${i + 1}`}
-                                    class="form-control" aria-describedby="button-addon2" value={recipeStep.step}
-                                    name="step" style={style} ref={el => this.textarea = el} rows={1} defaultValue="" onChange={event => this.handleStepChange(event, i)}>
-                                    </textarea>
-                                    <div class="input-group-append">
-                                       <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => this.handleRemoveStep(i)}>X</button>
-                                    </div>
-                                    </div>
-                                </div>
-                                </div>
-                                ))}
-                                   <button type="button" onClick={() => this.handleAddStep()}>
-                                   Add Step
-                                   </button>
-                            </form>
+                            <div class="modal-header">
+                                <h5 class="modal-title">Add recipe</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
                             </div>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" onClick={event => this.handleSubmit(event)}>Save changes</button>
-                          </div>
+                        <div class="modal-body">
+                            <div class="container-fluid">
+                                <form>
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="md-form">
+                                                <input type="text" id="name-form" placeholder="Name" class="form-control" name="name" onChange={event => this.handleNameChange(event)}/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                        {this.state.steps.map((recipeStep, i) => (
+                                            <div class="row">
+                                                <div class="next-input col-md-12">
+                                                    <div class="input-group mb-3">
+                                                        <textarea key={i} type="text" placeholder={`Step #${i + 1}`} class="next-step form-control" aria-describedby="button-addon2" value={recipeStep.step} name="step"  ref={el => this.textarea = el} rows={1} defaultValue="" onChange={event => this.handleStepChange(event, i)}>
+                                                        </textarea>
+                                                        <div class="form-group-append">
+                                                            <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => this.handleRemoveStep(i)}>X</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
+                                        <button type="button" class="btn btn-primary" onClick={() => this.handleAddStep()}>
+                                            Add step
+                                        </button>
+                                        </div>
+                                        <div class="col-md-6">
+                                        {this.state.ingredients.map((recipeIngredient, i) => (
+                                            <div class="next-input form-row">
+                                                <div class="form-group col-md-2">
+                                                    <input key={i} type="text" placeholder="Quantity" class="form-control" aria-describedby="button-addon2" value={recipeIngredient.quantity} name="quantity" onChange={event => this.handleIngredientChange(event, i)} />
+                                                </div>
+                                                <div class="form-group col-md-2">
+                                                    <select key={i} class="form-control" value={recipeIngredient.unit} name="unit" onChange={event => this.handleIngredientChange(event, i)}>
+                                                        <option selected>Unit</option>
+                                                        <option value="PIECE">piece</option>
+                                                        <option value="ML">ml</option>
+                                                        <option value="L">l</option>
+                                                        <option value="G">g</option>
+                                                        <option value="KG">kg</option>
+                                                        <option value="TSP">tsp</option>
+                                                        <option value="TBSP">tbsp</option>
+                                                        <option value="PINCH">pinch</option>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group col-md-7">
+                                                    <input key={i} type="text" placeholder="Ingredient" class="form-control" aria-describedby="button-addon2" value={recipeIngredient.ingredientType} name="ingredientType" onChange={event => this.handleIngredientChange(event, i)} />
+                                                </div>
+                                                <div class="form-group-append">
+                                                    <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => this.handleRemoveIngredient(i)}>X</button>
+                                                </div>
+                                            </div>
+                                        ))}
+                                        <button type="button" class="btn btn-primary" onClick={() => this.handleAddIngredient()}>
+                                            Add ingredient
+                                        </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                      </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" onClick={event => this.handleClose(event)} data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary" onClick={event => this.handleSubmit(event)}>Save recipe</button>
+                            </div>
+                        </div>
                     </div>
-
+                </div>
+                <div class="col-6">
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target={"#addRecipeModal"}>
                         Add recipe
                     </button>
-                    </React.Fragment>
-            )
+                </div>
+            </React.Fragment>
+        )
     }
 }
 
@@ -209,7 +246,7 @@ class ShowDetailsButton extends React.Component{
             var ingredient = ingredients[i];
             ingredientsList.push(
                 <li class="list-group-item">
-                    {ingredient.quantity} {ingredient.unit} <b>{ingredient.ingredientType.type}</b>
+                    {ingredient.quantity} {ingredient.unit} <b>{ingredient.ingredientType}</b>
                 </li>
             );
         }
@@ -275,7 +312,7 @@ class ShowDetailsButton extends React.Component{
 class RecipeList extends React.Component{
 	render() {
 		const recipes = this.props.recipes.map(r =>
-			<Recipe key={r.id} recipe={r}/>
+			<Recipe key={r.id} recipe={r} update={this.props.update}/>
 		);
 		return (
 		<div class="col-6">
